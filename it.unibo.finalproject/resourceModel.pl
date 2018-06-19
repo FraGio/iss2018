6575df268914579476fde770cc899e92fde2bcca
Unknown <giova6@hotmail.it>
2018-06-19 11:02:42 +0200

    Aggiunto on/off led in base a soglia temperatura

---
+++ it.unibo.finalproject/resourceModel.pl
@@ -0,0 +1,29 @@
+/*
+===============================================================
+resourceModel.pl
+===============================================================
+*/
+model( type(actuator, leds),      name(ledFisico), value(off) ).
+model( type(sensor, temperature), name(tempAmbiente),   value(25)  ).
+
+getModelItem( TYPE, CATEG, NAME, VALUE ) :-
+		model( type(TYPE, CATEG), name(NAME), value(VALUE) ).
+changeModelItem( CATEG, NAME, VALUE ) :-
+ 		replaceRule( 
+			model( type(TYPE, CATEG), name(NAME), value(_) ),  
+			model( type(TYPE, CATEG), name(NAME), value(VALUE) ) 		
+		),!,
+		%%output( changedModelAction(CATEG, NAME, VALUE) ),
+		( changedModelAction(CATEG, NAME, VALUE) %%to be defined by the appl designer
+		  ; true ).		%%to avoid the failure if no changedModelAction is defined
+		
+eval( ge, X, X ) :- !. 
+eval( ge, X, V ) :- eval( gt, X , V ) .
+ 
+emitevent( EVID, EVCONTENT ) :- 
+	actorobj( Actor ), 
+	%%output( emit( Actor, EVID, EVCONTENT ) ),
+	Actor <- emit( EVID, EVCONTENT ).
+%%%  initialize
+initResourceTheory :- output("initializing the initResourceTheory ...").
+:- initialization(initResourceTheory).
